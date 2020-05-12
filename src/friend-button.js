f13ecb145c8ae5ff6dd2cb9fcc4e27053312bc0a
Martin Paul <martinpaul.career@gmail.com>
2020-05-12 15:55:14 +0200

    part 8 added some (redundant, but whatever) friendshipt status info

--- src/friend-button.js
+++ src/friend-button.js
@@ -2,9 +2,8 @@ import React, { useState, useEffect } from "react";
 import axios from "./axios";
 //props from other-profile.js
 
-export default function FriendshipButton({ otherId }) {
-    const [buttonText, setButtonText] = useState("WANT SUM FUK?");
-
+export default function FriendshipButton({ otherId, first, last }) {
+    const [buttonText, setButtonText] = useState("");
     useEffect(() => {
         console.log("I am friendButton, hear me roar");
 
@@ -27,7 +26,20 @@ export default function FriendshipButton({ otherId }) {
 
     return (
         <div id="friend-info">
-            <p></p>
+            {buttonText == "Make Friend Request" && <p>Stranger</p>}
+            {buttonText == "Cancel Friend Request" && (
+                <p>You sent a Friend Request</p>
+            )}
+            {buttonText == "Accept Friend Request" && (
+                <p>
+                    {first} {last} sent a Friend Request
+                </p>
+            )}
+            {buttonText == "Unfriend" && (
+                <p>
+                    {first} {last} is your friend
+                </p>
+            )}
             <button className="btn" onClick={() => submit()}>
                 {buttonText}
             </button>
--- src/other-profile.js
+++ src/other-profile.js
@@ -46,6 +46,8 @@ export default class OtherProfile extends Component {
                         />
                         <FriendshipButton
                             otherId={this.props.match.params.id}
+                            first={first}
+                            last={last}
                         />
                     </div>
                 </div>
