232098517e177129d4e9c283e21d447db5020a45
Martin Paul <martinpaul.career@gmail.com>
2020-06-09 06:54:52 +0200

    hpm error with serving bundle.js

--- index.js
+++ index.js
@@ -7,9 +7,7 @@ const express = require("express");
 const app = express();
 //SOCKET.IO setup
 const server = require("http").Server(app); //socket.io needs a native node server (we can't use express's node server because express altered it a bit, so we use the Server constructor here and pass it "app" so that the express still works)
-const io = require("socket.io")(server, {
-    origins: "localhost:8080",
-});
+const io = require("socket.io")(server, { origins: "localhost:8080" });
 // const io = require("socket.io")(server, {
 //     origins: "localhost:8080 bc-socialnetwork.herokuapp.com:*",
 // }); //you need to pass socket.io the serv and origins with a list of host-names/ports that you will accept socket io connections from (there will be a http header with 'origin' value in it. it also prevents cross-site attacks. you separate multiple ports by a space)
@@ -523,7 +521,7 @@ app.get("*", function (req, res) {
 server.listen(8080, function () {
     console.log("socialnetwork server listening...");
 });
-// ////---------- socket.io setup
+
 // server.listen(process.env.PORT || 8080, function () {
 //     console.log("socialnetwork server listening...");
 // });
